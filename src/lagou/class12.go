32a6220f6bf72086ea8c9a1ec457a2506a7f808f
Abadbeginning <2029618609@qq.com>
2022-02-18 14:11:09 +0800

    12 | 指针详解：在什么情况下应该使用指针？

--- src/lagou/class12.go
+++ src/lagou/class12.go
@@ -2,21 +2,34 @@ package main
 
 import "fmt"
 
-func pointDemo()  {
+func pointDemo() {
 	age := 18
 	ageP := &age
 	var age1 *int
 	age1 = &age
-	fmt.Println(age1,*age1)
+	fmt.Println(age1, *age1)
 	fmt.Println("age值 --> ", age)
 	fmt.Println("age指针值 --> ", ageP)
 	fmt.Println("*age值 --> ", *ageP)
-	// var strP *string 
+	// var strP *string
 	// strP = &age
-	
+}
 
+func modifyName(name string) {
+	name = "pig"
+}
+func modifyName1(name *string) {
+	*name = "pig"
+}
+func showName() {
+	userName := "xiaoming"
+	modifyName(userName)
+	fmt.Println(userName)
+	modifyName1(&userName)
+	fmt.Println(userName)
 }
 
 func main() {
-	pointDemo()
+	//pointDemo()
+	showName()
 }
--- src/lagou/class8.go
+++ src/lagou/class8.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func goroutineDemo() {
@@ -24,77 +25,74 @@ func channelDemo() {
 	fmt.Println("接收值：", v)
 }
 
-func channel1Demo()  {
-	cacheCh:=make(chan int,5)
+func channel1Demo() {
+	cacheCh := make(chan int, 5)
 	cacheCh <- 2
 	cacheCh <- 3
-	fmt.Println("cacheCh容量为:",cap(cacheCh),",元素个数为：",len(cacheCh))
+	fmt.Println("cacheCh容量为:", cap(cacheCh), ",元素个数为：", len(cacheCh))
 }
 
-func channel2Demo()  {
-	func main() {
+func channel2Demo() {
 
-		//声明三个存放结果的channel
-	 
-		firstCh := make(chan string)
-	 
-		secondCh := make(chan string)
-	 
-		threeCh := make(chan string)
-	 
-		//同时开启3个goroutine下载
-	 
-		go func() {
-	 
-		   firstCh <- downloadFile("firstCh")
-	 
-		}()
-	 
-		go func() {
-	 
-		   secondCh <- downloadFile("secondCh")
-	 
-		}()
-	 
-		go func() {
-	 
-		   threeCh <- downloadFile("threeCh")
-	 
-		}()
-	 
-		//开始select多路复用，哪个channel能获取到值，
-	 
-		//就说明哪个最先下载好，就用哪个。
-	 
-		select {
-	 
-		case filePath := <-firstCh:
-	 
-		   fmt.Println(filePath)
-	 
-		case filePath := <-secondCh:
-	 
-		   fmt.Println(filePath)
-	 
-		case filePath := <-threeCh:
-	 
-		   fmt.Println(filePath)
-	 
-		}
-	 
-	 }
-	 
+	//声明三个存放结果的channel
+
+	firstCh := make(chan string)
+
+	secondCh := make(chan string)
+
+	threeCh := make(chan string)
+
+	//同时开启3个goroutine下载
+
+	go func() {
+
+		firstCh <- downloadFile("firstCh")
+
+	}()
+
+	go func() {
+
+		secondCh <- downloadFile("secondCh")
+
+	}()
+
+	go func() {
+
+		threeCh <- downloadFile("threeCh")
+
+	}()
+
+	//开始select多路复用，哪个channel能获取到值，
+
+	//就说明哪个最先下载好，就用哪个。
+
+	select {
+
+	case filePath := <-firstCh:
+
+		fmt.Println(filePath)
+
+	case filePath := <-secondCh:
+
+		fmt.Println(filePath)
+
+	case filePath := <-threeCh:
+
+		fmt.Println(filePath)
+
+	}
+
 }
 
 func downloadFile(chanName string) string {
-	 
+
 	//模拟下载文件,可以自己随机time.Sleep点时间试试
- 
+
 	time.Sleep(time.Second)
- 
-	return chanName+":filePath"
- 
- }
+
+	return chanName + ":filePath"
+
+}
 
 func main() {
 	//goroutineDemo()
